76c0618d0e0fceaed93bfcb32958d768062fd1e9
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2015-06-08 15:59:00 +0530

    Load comments from server

--- comments.js
+++ comments.js
@@ -4,11 +4,34 @@ var data = [
 ];
 
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return { data: [] };
+  },
+
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({ data: data });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+
   render: function() {
     return (
       <div className="commentBox">
       <h1>Comments</h1>
-      <CommentList data={this.props.data} />
+      <CommentList data={ this.state.data } />
       <CommentForm />
       </div>
     );
@@ -19,14 +42,14 @@ var CommentList = React.createClass({
   render: function() {
     var commentNodes = this.props.data.map(function (comment) {
       return (
-        <Comment author={comment.author}>
-        {comment.text}
+        <Comment author={ comment.author }>
+        { comment.text }
         </Comment>
       );
     });
     return (
       <div className="commentList">
-        { commentNodes }
+      { commentNodes }
       </div>
     );
   }
@@ -57,6 +80,6 @@ var Comment = React.createClass({
 });
 
 React.render(
-  <CommentBox data={ data } />,
+  <CommentBox url="http://localhost:8000/comments.json" pollInterval={ 2000 } />,
   document.getElementById('content')
 );
